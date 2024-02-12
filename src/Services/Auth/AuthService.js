b44edeb931b8d4b721da3d8cc2e0b0e38aa95a8f
whymassab <massab@vigoroustechnologies.com>
2024-02-13 04:37:39 +0500

    comment out

--- src/Services/Auth/AuthService.js
+++ src/Services/Auth/AuthService.js
@@ -1,11 +1,9 @@
 const AuthService = {
   logout() {
-    // Implement logout logic, e.g., clear local storage or session storage
     localStorage.removeItem('token');
     localStorage.removeItem('username');
   },
   isAuthenticated() {
-    // Implement logic to check if the user is authenticated, e.g., check if a token exists in local storage
     const token =  localStorage.getItem('token') !== null;
     return token;
   }
